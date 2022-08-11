456a32c66ce66bc66cd27f9636a571eed25c173e
Precious Nnam <nnamvannessa@gmail.com>
2022-08-12 00:29:55 +0100

    setting up project, first commit

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,20 @@
+import './category-item.styles.scss';
+
+//same as category.props ie the property passed to category on the app.js script
+const CategoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
