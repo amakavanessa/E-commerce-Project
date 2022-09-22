2bbc341bd5805843337eb3d67da2bfa251de7662
Precious Nnam <nnamvannessa@gmail.com>
2022-09-22 16:18:41 +0100

    create memorize selectors to optimize application

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,14 +1,20 @@
-// export const selectCategoriesMap = (state) =>
-//   state.categories.categories.reduce((acc, category) => {
-//     const { title, items } = category;
-//     acc[title.toLowerCase()] = items;
-//     return acc;
-//   }, {});
+import { createSelector } from 'reselect';
 
-export const selectCategoriesMap = (state) => {
-  console.log('selector fired');
-  return state.categories.categories.reduce((acc, { title, items }) => {
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
-};
+const selectCategoryReducer = (state) => state.categories;
+
+//Creating a memorize Selector
+export const selectCategories = createSelector(
+  //input selector
+  [selectCategoryReducer],
+  //output selector
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, { title, items }) => {
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
